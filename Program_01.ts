dd424e53f9d675f9da18427f72d709c56cdb45cf
Darshan Jain <116780686+Darshan2610@users.noreply.github.com>
2023-01-20 10:12:52 +0530

    Add files via upload

---
+++ Program_01.ts
@@ -0,0 +1,18 @@
+// let sname = "darshan";
+// console.log(sname);
+
+// var data: any[] = ["java", "python"];
+// console.log(data);
+
+// var a = 19;
+// if (a < 18) {
+//   console.log("kid");
+// } else {
+//   console.log("adult");
+// }
+
+function add(a: number, b: number) {
+  var c = a + b;
+  console.log(c);
+}
+add(10, 2);
